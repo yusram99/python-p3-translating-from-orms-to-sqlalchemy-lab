a576777f0ddab4345017548b8771e7b8a67a633a
Your Name <youremail@example.com>
2023-09-02 19:51:52 +0300

    commit message

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,31 @@
 from models import Dog
 
 def create_table(base):
-    pass
+    # Create the Dog table
+    base.metadata.create_all()
 
 def save(session, dog):
-    pass
+    # Save a dog to the database
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    # Retrieve all dogs from the database
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    # Find a dog by name
+    return session.query(Dog).filter_by(name=name).first()
 
 def find_by_id(session, id):
-    pass
+    # Find a dog by ID
+    return session.query(Dog).filter_by(id=id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    # Find a dog by name and breed
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    # Update the breed of a dog
+    dog.breed = breed
+    session.commit()
